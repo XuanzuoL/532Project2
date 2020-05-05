0bc92760673db4829f0e8cf46436374df5792585
XuanzuoL <xliu30@albany.edu>
2020-05-05 16:39:48 -0400

    remove /n

--- Project2/Task1.py
+++ Project2/Task1.py
@@ -6,7 +6,7 @@ f = open("/Users/christianliu/Desktop/CSI532/project/532projectdataset.txt", "r"
 for line in f:
     G.add_node(line.split(' ')[1])
     G.add_node(line.split(' ')[2])
-    G.add_edge(line.split(' ')[1], line.split(' ')[2])
+    G.add_edge(line.split(' ')[1], line.split(' ')[2].strip('\n'))
     # print(line.split(' ')[2])
 bidi_edge = 0
 visited_edge = []
@@ -18,7 +18,25 @@ for i in G.nodes():
                 bidi_edge += 1
 
 degree_vals = G.degree()
+in_degree_vals = G.in_degree()
+out_degree_vals = G.out_degree()
 deg_uniq = sorted(dict(degree_vals).values())
+deg_hist = [list(dict(degree_vals).values()).count(x) for x in deg_uniq]
+
+in_deg_uniq = sorted(dict(in_degree_vals).values())
+in_deg_hist = [list(dict(in_degree_vals).values()).count(x) for x in in_deg_uniq]
+
+out_deg_uniq = sorted(dict(out_degree_vals).values())
+out_deg_hist = [list(dict(out_degree_vals).values()).count(x) for x in out_deg_uniq]
+
+plt.figure()
+plt.loglog(deg_uniq, deg_hist, 'ro-')
+plt.loglog(in_deg_uniq, in_deg_hist, 'go-')
+plt.loglog(out_deg_uniq, out_deg_hist, 'yo-')
+plt.xlabel('Degree')
+plt.ylabel('Number of nodes')
+plt.show()
+
 print("Number of node is:",nx.number_of_nodes(G))
 print("Number of edge is:",nx.number_of_edges(G))
 print("Number of bidirectional edge is:",bidi_edge)
