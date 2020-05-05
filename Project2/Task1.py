636f3c77604f366e31f22aa591caa1f077e11993
XuanzuoL <xliu30@albany.edu>
2020-05-05 14:15:29 -0400

    a little change

--- Project2/Task1.py
+++ Project2/Task1.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.MultiDiGraph()
+G = nx.DiGraph()
 f = open("/Users/christianliu/Desktop/CSI532/project/532projectdataset.txt", "r", encoding='utf-8', errors='ignore')
 for line in f:
     G.add_node(line.split(' ')[1])
@@ -16,6 +16,7 @@ for i in G.nodes():
         if G.has_edge(i,j):
             if j not in visited_edge:
                 bidi_edge += 1
+
 degree_vals = G.degree()
 deg_uniq = sorted(dict(degree_vals).values())
 print("Number of node is:",nx.number_of_nodes(G))
@@ -24,4 +25,5 @@ print("Number of bidirectional edge is:",bidi_edge)
 print("Min number of other addresses each email address has interacted with is:",min(deg_uniq))
 print("Max number of other addresses each email address has interacted with is:",max(deg_uniq))
 print("Ave number of other addresses each email address has interacted with is:",sum(deg_uniq)/len(deg_uniq) )
-# print("Diameter of the network is:",nx.diameter(G))
+print("Diameter of the network is:",nx.diameter(G))
+
